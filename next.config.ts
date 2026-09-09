34f91a0c492b116dbfb2fe8f4c56548c8d2e5711
Jeff <lv5.accelerator@gmail.com>
2026-09-08 20:59:36 -0400

    feat(info): allow CommunityDragon detail assets

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "raw.communitydragon.org",
-        pathname: "/latest/game/assets/characters/**",
+        pathname: "/latest/game/**",
       },
     ],
   },
